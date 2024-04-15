697a517fec58384177189c94d9d345975fd7d890
Satty Kaur <satty.k@weareframework.co.uk>
2024-04-15 15:14:09 +0100

    add authentication via breeze

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use OpenAI\Laravel\Facades\OpenAI;
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 });
 
-Route::get('/test', function () {
-    $result = OpenAI::chat()->create([
-//        'model' => 'gpt-3.5-turbo',
-//        'messages' => [
-//            ['role' => 'user', 'content' => 'Hello!'],
-//        ],
-        'model' => 'gpt-4',
-        'messages' => [
-            ['role' => 'user', 'content' => 'do you like pina coladas?',]
-        ],
-    ]);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-    return $result->choices[0]->message->content; // Hello! How can I assist you today?
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
+require __DIR__.'/auth.php';
